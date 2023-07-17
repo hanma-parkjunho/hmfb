9b9617fc2dc545ac3a7a5d13479902c70810511d
rlarmsgh153 <75015149+rlarmsgh153@users.noreply.github.com>
2023-07-17 17:09:08 +0900

    test

--- hmfb-core/src/main/java/hmfb/core/dto/F2000101Dto.java
+++ hmfb-core/src/main/java/hmfb/core/dto/F2000101Dto.java
@@ -7,27 +7,27 @@ import lombok.Data;
 
 /**
  * @author USER
- * 업무개시
+ * 당,타행지급(자금)이체
+ * 김준영
  */
 @Data
 public class F2000101Dto implements BaseMessage{
-	@FixedString(order = 1, type = MessageFieldType.ALPHABET, value = 6)
-	private String telemsgNo;			// 식별번호
+	// test
 	@FixedString(order = 1, type = MessageFieldType.ALPHABET, value = 15)
-	private String pymentAcunt; 		// 지급계좌
+	private String PymntAcnut; 			// 지급계좌
 	@FixedString(order = 2, type = MessageFieldType.ALPHABET, value = 8)
 	private String password; 			// 비밀번호
-	@FixedString(order = 3, type = MessageFieldType.NUMERIC, value = 6)
+	@FixedString(order = 3, type = MessageFieldType.ALPHABET, value = 6)
 	private String revwSmbol; 			// 복기부호
-	@FixedString(order = 4, type = MessageFieldType.ALPHABET, value = 13)
-	private String pymntMount; 			// 지급금액
+	@FixedString(order = 4, type = MessageFieldType.NUMERIC, value = 13)
+	private String PymntAmount; 		// 지급금액
 	@FixedString(order = 5, type = MessageFieldType.ALPHABET, value = 1)
 	private String smbol; 				// 부호
 	@FixedString(order = 6, type = MessageFieldType.NUMERIC, value = 13)
 	private String blce; 				// 잔액
-	@FixedString(order = 7, type = MessageFieldType.NUMERIC, value = 3)
+	@FixedString(order = 7, type = MessageFieldType.ALPHABET, value = 3)
 	private String bankCode; 			// 은행코드
-	@FixedString(order = 8, type = MessageFieldType.NUMERIC, value = 14)
+	@FixedString(order = 8, type = MessageFieldType.ALPHABET, value = 15)
 	private String rcpmnyAcnut; 		// 입금계좌
 	@FixedString(order = 9, type = MessageFieldType.NUMERIC, value = 9)
 	private String fee; 				// 수수료
@@ -41,23 +41,7 @@ public class F2000101Dto implements BaseMessage{
 	private String dpstrNm; 			// 예금주명
 	@FixedString(order = 14, type = MessageFieldType.ALPHABET, value = 2)
 	private String cptalCharct; 		// 자금성격
-	
-	@FixedString(order = 15, type = MessageFieldType.ALPHABET, value = 5)
-	private String filler1; 			// 예비1
-	@FixedString(order = 16, type = MessageFieldType.ALPHABET, value = 5)
-	private String filler2; 			// 예비2
-	@FixedString(order = 17, type = MessageFieldType.ALPHABET, value = 5)
-	private String filler3; 			// 예비3
-	@FixedString(order = 18, type = MessageFieldType.ALPHABET, value = 5)
-	private String filler4; 			// 예비4
-	@FixedString(order = 19, type = MessageFieldType.ALPHABET, value = 5)
-	private String filler5; 			// 예비5
-	
-	@FixedString(order = 20, type = MessageFieldType.ALPHABET, value = 4)
-	private String rspnsCode; 			// 응답코드
-	@FixedString(order = 21, type = MessageFieldType.ALPHABET, value = 500)
-	private String rspnsMssage;			// 응답메세지
-	@FixedString(order = 22, type = MessageFieldType.ALPHABET, value = 2)
-	private String sendCode;			// 이체전송여부
+	@FixedString(order = 15, type = MessageFieldType.ALPHABET, value = 60)
+	private String filler;				// 예비영역
 		
 }
